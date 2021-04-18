b92a7c649c1b57fda8e99d672864ab5fd8d8a16f
sdonne <stefanodonne@gmail.com>
2021-04-18 15:45:03 +0200

    Implements brute solution with glpk

--- src/flp.py
+++ src/flp.py
@@ -28,10 +28,8 @@ def read_instance(file_name):
                 travel_cost[(i, j)] = int(info[j])
     except:
         print("Error reading file.")
-    return opening_cost, demand, capacity, travel_cost
-
-
-# cost to open each factory, demand of each customer, capacity of each factory, travel cost to customer i from factory j
+    return opening_cost, demand, capacity, travel_cost  # cost to open each factory, demand of each customer
+    # capacity of each factory, travel cost to customer i from factory j
 
 
 def test():  # test pyomo concrete model functions
@@ -46,7 +44,7 @@ def test():  # test pyomo concrete model functions
 
 def solve_flp(instance_name, linear):
     instance_param = read_instance(instance_name)
-    print(instance_param[0])
+
     model = pyo.ConcreteModel()
     model.I = pyo.RangeSet(0, len(instance_param[1]) - 1)
     model.J = pyo.RangeSet(0, len(instance_param[2]) - 1)
@@ -56,22 +54,44 @@ def solve_flp(instance_name, linear):
     model.t = pyo.Param(model.I, model.J, initialize=instance_param[3], default=0)  # cost to move 1 unit from J to I
 
     model.x = pyo.Var(model.I, model.J,
-                      domain=pyo.PositiveIntegers)  # integer amount of client i that is satisfied by facility j
-    model.y = pyo.Var(model.J, domain=pyo.Binary)  # yj = 1 if factory j is built
+                      domain=pyo.NonNegativeIntegers)  # integer amount of client i demand that is satisfied by facility j
+    model.y = pyo.Var(model.J, domain=pyo.Binary)  # yj = 1 if factory j is built, 0 else
 
-    def obj_rule(model):
-        return (pyo.summation(model.f, model.y)  # cost of opening all facilities
-                + pyo.summation(model.t, model.x)  # cost of moving units from facilities to clients
-                )
+    def obj_rule(_model):
+        return (
+                pyo.summation(_model.f, _model.y)  # cost of opening facilities
+                + pyo.summation(_model.t, _model.x)  # cost of moving units from facilities to clients
+        )
         # sum(instance_name[0][j] * model.y[j] for j in model.J) + sum(
         #             instance_name[3][i, j] * model.x[i, j] for i in model.I for j in model.J)
 
-    model.obj = pyo.Objective(rule=obj_rule, sense=pyo.minimize)
-    # TODO : ADD CONSTRAINTS
+    model.obj = pyo.Objective(rule=obj_rule, sense=pyo.minimize)  # minimize the cost of operation
+
+    def cst1(_model, j):
+        return (
+                sum(_model.x[i, j] for i in _model.I) <= _model.y[j] * _model.c[j]
+        )
+
+    model.constraint_1 = pyo.Constraint(model.J, rule=cst1)  # the sum of the units sent from one factory to all its
+
+    # customer cannot be above its capacity (if it's open)
+
+    def cst2(_model, i):
+        return (
+                sum(_model.x[i, j] for j in _model.J) >= _model.d[i]
+        )
+
+    model.constraint_2 = pyo.Constraint(model.I, rule=cst2)  # for each customer all the demand must be satisfied
+
     opt = pyo.SolverFactory('glpk')
     opt.solve(model, tee=True)
+    # print results
     print(pyo.value(model.obj))
-    # return (obj,x,y)
+    for i in model.x:
+        print(str(model.x[i]), model.x[i].value)
+    for j in model.y:
+        print(str(model.y[j]), model.y[j].value)
+        # return (obj,x,y)
 
 
 def initial_solution_flp(instance_name):
